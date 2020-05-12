f2020879822db422e0c55d43b520ae56ecfa8747
Glauber M. da Silva <gmsoft.informatica@gmail.com>
2020-05-12 14:14:39 -0300

    Adicionado django_heroku settings

--- blog_web/settings.py
+++ blog_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +129,4 @@ MEDIA_ROOT = (
 )
 
 MEDIA_URL = '/media/' #endereço para acessar os arquivos
+django_heroku.settings(locals())
